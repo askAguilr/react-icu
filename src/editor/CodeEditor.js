90f446375c4bf766e97fc2e2fcd25314bc1bc8fa
askAguilr <dafsoft@gmail.com>
2019-08-04 02:17:47 -0500

    code tab

---
+++ src/editor/CodeEditor.js
@@ -0,0 +1,34 @@
+import React, { useRef, useState } from "react";
+import Editor from "@monaco-editor/react";
+ 
+const CodeEditor= () =>{
+  const [isEditorReady, setIsEditorReady] = useState(false);
+  const valueGetter = useRef();
+ 
+  function handleEditorDidMount(_valueGetter) {
+    setIsEditorReady(true);
+    valueGetter.current = _valueGetter;
+  }
+ 
+  function handleShowValue() {
+    alert(valueGetter.current());
+  }
+ 
+  return (
+    <>
+      <button onClick={handleShowValue} disabled={!isEditorReady}>
+        Show value
+      </button>
+ 
+      <Editor
+        height="90vh"
+        language="javascript"
+        theme='dark'
+        value={"// write your code here"}
+        editorDidMount={handleEditorDidMount}
+      />
+    </>
+  );
+}
+
+export default CodeEditor;
--- src/editor/Editor.js
+++ src/editor/Editor.js
@@ -1,5 +1,5 @@
 import React,{useState,useEffect} from 'react';
-import {Navbar,Nav,Form,FormControl,Button} from 'react-bootstrap';
+import {Navbar,Nav,Form,FormControl,Button, ButtonGroup} from 'react-bootstrap';
 
 import grapesjs from 'grapesjs';
 import 'grapesjs/dist/css/grapes.min.css';
@@ -7,16 +7,19 @@ import blocks from './blocks'
 import style from './style'
 import storage from  './storage'
 import {plugins,pluginsOpts} from './plugins'
+import CodeEditor from './CodeEditor'
 
 let editor;
 function Editor() {
+  const [mode,setMode] = useState('design');
+
   useEffect(()=>{
     editor = grapesjs.init({
         container: '#gjs',
         canvas:{
           styles:['https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css']
         },
-        fromElement: true,
+        fromElement: false,
         storageManager: storage,
         panels: { defaults: [] },
         blockManager: {
@@ -38,7 +41,7 @@ function Editor() {
         pluginsOpts: pluginsOpts
         
       });
-  });
+  },[]);
 
   const handleExport = ()=>{
       console.log();
@@ -50,32 +53,44 @@ function Editor() {
   }
   
   return (
-    <div id="editor">
+    <>
       <Navbar id="navbar" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">ReactWeaver</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link onClick={handleNew}>New</Nav.Link>
+          <Navbar.Brand href="#home">ReactWeaver</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link onClick={handleNew}>New</Nav.Link>
 
-          <Button variant="secondary">Initialize</Button>
-        </Nav>
-        <Nav style={{float:'right'}}>
-          <Button onClick={handleExport} variant="outline-light">Export</Button>
-        </Nav>
-      </Navbar>
-      <div id='main'>
-        <div id='left'>
-            <div className="layers-container"></div>
-        </div>
-        <div id="gjs">
-            <h1>Hello World Component!</h1>
-        </div>
-        <div id='right'>
-            <div className="layers-container"></div>
-            <div className="styles-container"></div>
-        </div>
+            <Button variant="secondary">Initialize</Button>
+          </Nav>
+          <Nav className="mr-auto">
+           <ButtonGroup aria-label="View mode">
+              <Button onClick={setMode.bind(this,'design')} variant="outline-light">Design</Button>
+              <Button onClick={setMode.bind(this,'code')}  variant="outline-light">Code</Button>
+            </ButtonGroup>
+          </Nav>
+          <Nav style={{float:'right'}}>
+            <Button onClick={handleExport} variant="outline-light">Export</Button>
+          </Nav>
+        </Navbar>
+      <div id="design" style={{display:mode==='design'?'block':'none'}}>
+          <div id='main'>
+            <div id='left'>
+                <div className="layers-container"></div>
+            </div>
+            <div id="gjs">
+            </div>
+            <div id='right'>
+                <div className="layers-container"></div>
+                <div className="styles-container"></div>
+            </div>
+          </div>
+          <div id="blocks"></div>
       </div>
-      <div id="blocks"></div>
-    </div>
+      <div id="code" style={{display:mode==='code'?'block':'none'}}>
+   
+          <CodeEditor/>
+   
+      </div>
+    </>
   );
 }
 
